7f097729179b6bfc8cebad92dcb86e7c0a54db0c
Anugerah Erlaut <aerlaut@live.com>
2020-09-18 21:06:09 +0700

    delete topics

--- controllers/TopicController.js
+++ controllers/TopicController.js
@@ -36,55 +36,49 @@ exports.update = (req, res) => {
 	})
 
 	// Get current list of topics
-	const currentTags = Topic.find(
-		{ name: { $in: inputTopics } },
-		{ name: 1 },
-		(err, docs) => {
+	const currentTags = Topic.find({}, { name: 1 }, (err, docs) => {
+		if (err) {
+			return res.status(500).json(err)
+		}
+
+		docs = docs.map((el) => el.name)
+
+		// add new topics
+		let addTopics = inputTopics.filter((topic) => {
+			return !docs.includes(topic)
+		})
+
+		// Delete topics
+		let deleteTopics = docs.filter((topic) => {
+			return !inputTopics.includes(topic)
+		})
+
+		// Create the new input array
+		let newTopics = [...topics, ...subtopics].filter((el) => {
+			if (addTopics.includes(el.name)) {
+				return el
+			}
+		})
+
+		// Delete old topics
+		if (deleteTopics.length > 0) {
+			Topic.deleteMany({ name: { $in: deleteTopics } }, (err) => {
+				if (err) {
+					return res.status(500).json(err)
+				}
+			})
+		}
+
+		// Add new topics
+		Topic.insertMany(newTopics, (err, docs) => {
 			if (err) {
 				return res.status(500).json(err)
 			}
 
-			docs = docs.map((el) => el.name)
-
-			// add new topics
-			let addTopics = inputTopics.filter((topic) => {
-				return !docs.includes(topic)
+			return res.json({
+				status: 'ok',
+				message: 'Topics inserted',
 			})
-
-			// Delete topics
-			let deleteTopics = docs.filter((topic) => {
-				return !inputTopics.includes(topic)
-			})
-
-			// Create the new input array
-			let newTopics = [...topics, ...subtopics].filter((el) => {
-				if (addTopics.includes(el.name)) {
-					return el
-				}
-			})
-
-			// Delete old topics
-			if (deleteTopics.length > 0) {
-				Topic.deleteMany({ name: { $in: deleteTopics } }, (err) => {
-					if (err) {
-						return res.status(500).json(err)
-					}
-				})
-			}
-
-			console.log(newTopics)
-
-			// Add new topics
-			Topic.insertMany(newTopics, (err, docs) => {
-				if (err) {
-					return res.status(500).json(err)
-				}
-
-				return res.json({
-					status: 'ok',
-					message: 'Topics inserted',
-				})
-			})
-		}
-	)
+		})
+	})
 }
